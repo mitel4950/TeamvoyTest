9df1bfed6bae37156579650deb9c1f98da9b8885
Olex Mashchenko <mitel4950@gmail.com>
2023-09-29 18:10:10 +0300

    Initial commit

---
+++ src/main/java/com/example/teamvoytest/TeamvoyTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.teamvoytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamvoyTestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TeamvoyTestApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/teamvoytest/TeamvoyTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teamvoytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamvoyTestApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
